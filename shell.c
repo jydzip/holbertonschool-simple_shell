74587cede62a7a1cc37e1efca322bd4af27335a3
my-holber <8762@holbertonstudents.com>
2024-04-20 17:29:33 +0200

    Added execute_cmd function for executing commands in the shell.

--- shell.c
+++ shell.c
@@ -1,36 +1,34 @@
 #include "shell.h"
 
-void execute_cmd(char **tokens, char **env, char *name_execute)
-{
-	int status;
-	pid_t child_pid;
-	char *cmd = get_path(env, tokens[0]);
+void execute_cmd(char **tokens, char **env, char *name_ex) {
+    int status;
+    pid_t child_pid;
+    char *cmd;
 
-	if (!cmd)
-	{
-		fprintf(stderr, "%s: No such file or directory\n", name_execute);
-		return;
-	}
+    if (tokens[0][0] == '/')
+        cmd = tokens[0];
+    else
+        cmd = get_path(env, tokens[0]);
 
-	child_pid = fork();
-	tokens[0] = cmd;
+    if (!cmd) {
+        fprintf(stderr, "%s: No such file or directory\n", tokens[0]);
+        return;
+    }
 
-	if (child_pid == -1)
-	{
-		fprintf(stderr, "Error: Fork failed\n");
-		free(cmd);
-		exit(EXIT_FAILURE);
-	}
-	else if (child_pid > 0)
-	{
-		waitpid(child_pid, &status, 0);
-		free(cmd);
-	}
-	else
-	{
-		execve(tokens[0], tokens, NULL);
-		fprintf(stderr, "Error: execve failed\n");
-                free(cmd);
-                exit(EXIT_FAILURE);
-	}
+    child_pid = fork();
+    tokens[0] = cmd;
+
+    if (child_pid == -1) {
+        fprintf(stderr, "Error: Fork failed\n");
+        free(cmd);
+        exit(EXIT_FAILURE);
+    } else if (child_pid > 0) {
+        waitpid(child_pid, &status, 0);
+        free(cmd);
+    } else {
+        execve(tokens[0], tokens, NULL);
+        fprintf(stderr, "Error: execve failed\n");
+        free(cmd);
+        exit(EXIT_FAILURE);
+    }
 }
