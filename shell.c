e2c4077731e5391e7a47ff44cf0ec6e77ce28ed0
jydzip <dckjeremy.zip@gmail.com>
2024-04-20 17:49:45 +0200

    Fix function execute_cmd leak memory with direct method path

--- shell.c
+++ shell.c
@@ -1,34 +1,41 @@
 #include "shell.h"
 
 void execute_cmd(char **tokens, char **env, char *name_execute) {
-    int status;
-    pid_t child_pid;
-    char *cmd;
+	int status;
+	pid_t child_pid;
+	char *cmd;
+	int direct_method = 0;
 
-    if (tokens[0][0] == '/')
-        cmd = tokens[0];
-    else
-        cmd = get_path(env, tokens[0]);
+	if (tokens[0][0] == '/')
+	{
+		direct_method = 1;
+		cmd = tokens[0];
+	}
+	else
+		cmd = get_path(env, tokens[0]);
 
-    if (!cmd) {
-        fprintf(stderr, "%s: No such file or directory\n", name_execute);
-        return;
-    }
+	if (!cmd) {
+		fprintf(stderr, "%s: No such file or directory\n", name_execute);
+		return;
+	}
 
-    child_pid = fork();
-    tokens[0] = cmd;
+	child_pid = fork();
+	tokens[0] = cmd;
 
-    if (child_pid == -1) {
-        fprintf(stderr, "Error: Fork failed\n");
-        free(cmd);
-        exit(EXIT_FAILURE);
-    } else if (child_pid > 0) {
-        waitpid(child_pid, &status, 0);
-        free(cmd);
-    } else {
-        execve(tokens[0], tokens, NULL);
-        fprintf(stderr, "Error: execve failed\n");
-        free(cmd);
-        exit(EXIT_FAILURE);
-    }
+	if (child_pid == -1) {
+		fprintf(stderr, "Error: Fork failed\n");
+		if (direct_method == 0)
+			free(cmd);
+		exit(EXIT_FAILURE);
+	} else if (child_pid > 0) {
+		waitpid(child_pid, &status, 0);
+		if (direct_method == 0)
+			free(cmd);
+	} else {
+		execve(tokens[0], tokens, NULL);
+		fprintf(stderr, "Error: execve failed\n");
+		if (direct_method == 0)
+			free(cmd);
+		exit(EXIT_FAILURE);
+	}
 }
