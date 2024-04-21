fda50e9b0b59a1796ab40fe5e20351849b56f8a1
jydzip <dckjeremy.zip@gmail.com>
2024-04-21 14:38:07 +0200

    Fix execute_cmd function, add check if waitpid error and remove free condition cmd

--- shell.c
+++ shell.c
@@ -4,17 +4,14 @@ void execute_cmd(char **tokens, char **env, char *name_execute) {
 	int status;
 	pid_t child_pid;
 	char *cmd;
-	int direct_method = 0;
 
 	if (tokens[0][0] == '/')
-	{
-		direct_method = 1;
-		cmd = tokens[0];
-	}
+		cmd = strdup(tokens[0]);
 	else
 		cmd = get_path(env, tokens[0]);
 
-	if (!cmd) {
+	if (!cmd)
+	{
 		fprintf(stderr, "%s: 1: %s: not found\n", name_execute, tokens[0]);
 		return;
 	}
@@ -22,20 +19,20 @@ void execute_cmd(char **tokens, char **env, char *name_execute) {
 	child_pid = fork();
 	tokens[0] = cmd;
 
-	if (child_pid == -1) {
+	if (child_pid == -1)
+	{
+		free(cmd);
 		fprintf(stderr, "Error: Fork failed\n");
-		if (direct_method == 0)
-			free(cmd);
-		exit(EXIT_FAILURE);
-	} else if (child_pid > 0) {
-		waitpid(child_pid, &status, 0);
-		if (direct_method == 0)
-			free(cmd);
-	} else {
-		execve(tokens[0], tokens, env);
-		fprintf(stderr, "Error: execve failed\n");
-		if (direct_method == 0)
-			free(cmd);
 		exit(EXIT_FAILURE);
 	}
+	else if (child_pid == 0)
+		execve(tokens[0], tokens, env);
+
+	if (waitpid(child_pid, &status, 0) == -1)
+	{
+		free(cmd);
+		fprintf(stderr, "Error: waitpid failed\n");
+		exit(EXIT_FAILURE);
+	}
+	free(cmd);
 }
