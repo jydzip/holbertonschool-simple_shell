3f74041747c2568a39e366c423d7fbdb7af81c63
jydzip <dckjeremy.zip@gmail.com>
2024-04-22 17:20:40 +0200

    Fix data type return of function execute_cmd()

--- shell.c
+++ shell.c
@@ -8,9 +8,8 @@
  * @cmd_count: The number of commands executed so far
  *
  * Return: Void
- */
-
- execute_cmd(char *line, char **env, char *name_execute, int cmd_count) {
+*/
+int execute_cmd(char *line, char **env, char *name_execute, int cmd_count) {
 	int status;
 	pid_t child_pid;
 	char *cmd;
