15a2add19a7dcfa98a518c3173aeff19d205a1b7
my-holber <8762@holbertonstudents.com>
2024-04-25 21:58:25 +0200

    i fixed error

--- split_line.c
+++ split_line.c
@@ -1,18 +1,28 @@
-#ifndef SHELL_H
-#define SHELL_H
+include "shell.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+/**
+ * split_line - Split a line into words
+ * @line: The line to split
+ *
+ * Return: An array of strings containing the words, or NULL on failure
+ */
+char **split_line(char *line)
+{
+	char **words = NULL;
+	int word_count = 0;
+	const char *delimiter = " \t\n";
+	char *token = strtok(line, delimiter);
 
-char *get_path(char **env, const char *cmd_name);
-char *read_line(void);
-char **split_line(char *line);
-int execute_cmd(char *line, char **env, char *name_execute, int cmd_count);
-void print_environment(char **env);
-void ctrl_Z_handler(void);
+	words = malloc(128 * sizeof(char *));
+	if (words == NULL)
+		return (NULL);
 
-#endif
+	while (token != NULL)
+	{
+		words[word_count] = token;
+		word_count++;
+		token = strtok(NULL, delimiter);
+	}
+	words[word_count] = NULL;
+	return (words);
+}
